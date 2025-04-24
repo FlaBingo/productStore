5dca978c87b8a3780b4d142623cdde786954b621
Satyam Prajapati <satyamm1402@gmail.com>
2025-04-24 07:47:33 +0530

    implement logout functionality; clear JWT cookie and handle response messages

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -120,6 +120,15 @@ export const login = async (req, res) => {
     }
 }
 
-export const logout = () => {
-    
+export const logout = (req, res) => {
+    try {
+        res.clearCookie("jwt-token");
+        res.status(200).json({success: true, message: "Logged out successfully"})
+    } catch (error) {
+        console.log("Error in logout controller ", error.message)
+        res.status(500).json({
+            success: false,
+            message: "Internal server error"
+        })
+    }
 }
