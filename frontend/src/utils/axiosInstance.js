ca5417eb32e47793629afa906ab03bd1b5f4ecb4
Satyam Prajapati <satyamm1402@gmail.com>
2025-05-02 19:23:48 +0530

    fix: Update axios instance baseURL to use VITE_API_URL environment variable for production

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = process.env.NODE_ENV === "production" ? "https://product-store-kappa-tawny.vercel.app//api" : "http://localhost:5000/api";
+const baseURL = process.env.NODE_ENV === "production" ? process.env.VITE_API_URL : "http://localhost:5000/api";
 
 const axiosInstance = axios.create({
     baseURL,
