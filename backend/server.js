45aa74c7e6bcf1f8e059333ce7554948ae2b7596
Satyam Prajapati <satyamm1402@gmail.com>
2025-05-02 19:40:29 +0530

    fix: Simplify CORS origin configuration for production and development environments

--- backend/server.js
+++ backend/server.js
@@ -14,8 +14,8 @@ const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve(); // For Production
 app.use(cors({
     origin: process.env.NODE_ENV === 'production'
-      ? ['https://product-store-kappa-tawny.vercel.app']
-      : ['http://localhost:5173'],
+      ? 'https://product-store-kappa-tawny.vercel.app'
+      : 'http://localhost:5173',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
