bd137660c84da2a458a40d4a8deddf942a1badf8
Satyam Prajapati <satyamm1402@gmail.com>
2025-05-02 23:17:09 +0530

    fix: Add exposedHeaders for CORS configuration to allow set-cookie header

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,8 @@ app.use(cors({
     origin: process.env.CLIENT_URL || "http://localhost:5173",
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    exposedHeaders: ['set-cookie']
   }));
 app.use(cookieParser())
 app.use(express.urlencoded({extended: true}))
