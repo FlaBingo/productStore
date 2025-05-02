858dc9f18010321607b0499d8e88713823cba364
Satyam Prajapati <satyamm1402@gmail.com>
2025-05-02 18:58:23 +0530

    fix: Update production URLs in CORS and axios instance for correct API endpoint

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve(); // For Production
 app.use(cors({
     origin: process.env.NODE_ENV === 'production'
-      ? ['https://your-vercel-url.vercel.app']
+      ? ['https://product-store-kappa-tawny.vercel.app']
       : ['http://localhost:5173'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
