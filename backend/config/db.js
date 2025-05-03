3af6267157339c444c7a83728e3bf1781f534fea
Satyam Prajapati <satyamm1402@gmail.com>
2025-05-03 07:45:20 +0530

    still testing but this should work

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,13 @@ import mongoose from "mongoose"
 
 export const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            serverSelectionTimeoutMS: 20000, // Timeout after 20s instead of 10s
+            socketTimeoutMS: 45000, // Close sockets after 45s
+            connectTimeoutMS: 20000, // Give up initial connection after 20s
+            maxPoolSize: 50, // Maintain up to 50 socket connections
+            family: 4 // Use IPv4, skip trying IPv6
+        });
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
